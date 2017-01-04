34d8e0477c71c1dc61f701a669d498dadc481004
generakthink <general_think@163.com>
2017-01-04 17:12:23 +0800

    引用高版本的Junit包进行测试

--- src/test/java/com/generalthink/common/datastructure/MyArrayListTest.java
+++ src/test/java/com/generalthink/common/datastructure/MyArrayListTest.java
@@ -1,20 +1,24 @@
 package com.generalthink.common.datastructure;
 
-import junit.framework.TestCase;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 
-public class MyArrayListTest extends TestCase{
 
-  
+public class MyArrayListTest{
+
+  @Test
   public void testAdd() {
     MyArrayList<Integer> list = new MyArrayList<Integer>(20);
     for(int i = 0;i < 30;i++) {
       list.add(i,i);
     }
     assertEquals(list.size(), 30);
+    
     System.out.println("testAdd success");
   }
-  
+  @Test
   public void testRemove() {
     MyArrayList<Integer> list = new MyArrayList<Integer>();
     for(int i = 0;i < 6;i++) {
@@ -24,9 +28,8 @@ public class MyArrayListTest extends TestCase{
     list.remove(4);
     list.remove(4);
     assertEquals(list.toString(), "[0, 1, 2]");
-    System.out.println("testRemove success");
   }
-  
+  @Test
   public void testGet() {
     MyArrayList<Integer> list = new MyArrayList<Integer>(-2);
     for(int i = 0;i < 10;i++) {
@@ -37,9 +40,9 @@ public class MyArrayListTest extends TestCase{
     assertEquals(list.get(3).intValue(), 3);
     assertEquals(list.get(9).intValue(), 9);
     
-    System.out.println("testGet success");
   }
   
+  @Test
   public void testSet() {
     MyArrayList<Integer> list = new MyArrayList<Integer>();
     list.add(0,-1);
@@ -48,7 +51,6 @@ public class MyArrayListTest extends TestCase{
     list.set(0, 0);
     list.set(1, 2);
     assertTrue(list.get(1) == 2);
-    System.out.println("testSet success");
   }
   
 }
