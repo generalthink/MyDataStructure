9be06be84b872d0f01482217d9ca77d16a153651
TKing <general_go@163.com>
2017-01-07 23:44:47 +0800

    添加MyLinkedList测试类,测试了MyLinkedList中方法

---
+++ src/test/java/com/generalthink/common/datastructure/MyLinkedListTest.java
@@ -0,0 +1,91 @@
+package com.generalthink.common.datastructure;
+
+import static org.junit.Assert.*;
+
+import java.util.Iterator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MyLinkedListTest {
+
+  private MyLinkedList<Integer> list;
+  
+  @Before
+  public void setUp() throws Exception {
+    
+    list = new MyLinkedList<Integer>();
+    for(int i = 0;i < 5;i++) {
+      list.add(i,i);
+    }
+  }
+
+  @Test
+  public void testAdd() {
+    list.add(5, 5);
+    list.add(0, -1);
+    assertTrue(list.size() == 7);
+    assertTrue(list.toString().equals("[-1,0,1,2,3,4,5]"));
+  }
+
+  @Test
+  public void testGet() {
+    assertTrue(list.get(0).equals(0));
+    assertTrue(list.get(1).equals(1));
+    assertTrue(list.get(2).equals(2));
+    assertTrue(list.get(3).equals(3));
+    assertTrue(list.get(4).equals(4));
+    try {
+      list.get(5);
+    } catch (IndexOutOfBoundsException e) {
+      fail("IndexOutOfBounds");
+    }
+    
+  }
+  
+  @Test
+  public void testRemove() {
+    list.remove(2);
+    assertTrue(list.size() == 4);
+    assertTrue(list.get(0) == 0);
+    assertTrue(list.get(1) == 1);
+    assertTrue(list.get(2) == 3);
+    assertTrue(list.get(3) == 4);
+  }
+  
+  @Test
+  public void testSet() {
+    list.set(0, -1);
+    assertTrue(list.size() == 5);
+    assertTrue(list.get(0) == -1);
+    try {
+      list.set(5,5);
+    } catch (Exception e) {
+      fail("IndexOutOfBounds");
+    }
+  }
+  
+  @Test
+  public void testIterator() {
+    Iterator<Integer> it = list.iterator();
+    try {
+      it.next();
+    } catch (Exception e) {
+      fail(e.toString());
+    }
+  }
+  
+  @Test
+  public void testIteratorModifi() {
+    Iterator<Integer> it = list.iterator();
+    while(it.hasNext()) {
+      System.out.println(it.next());
+      try {
+        list.add(0, 10);
+      } catch (Exception e) {
+        fail("ConcurrentModifiy Exception");
+      }
+    }
+  }
+  
+}
