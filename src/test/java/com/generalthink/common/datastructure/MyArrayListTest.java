b1d2ef88c4a2585ef637dacccda09d64d35b63fb
generakthink <general_think@163.com>
2017-01-10 13:34:33 +0800

    格式化代码

--- src/test/java/com/generalthink/common/datastructure/MyArrayListTest.java
+++ src/test/java/com/generalthink/common/datastructure/MyArrayListTest.java
@@ -4,53 +4,53 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+public class MyArrayListTest {
 
+	@Test
+	public void testAdd() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>(20);
+		for (int i = 0; i < 30; i++) {
+			list.add(i, i);
+		}
+		assertEquals(list.size(), 30);
+	}
 
-public class MyArrayListTest{
+	@Test
+	public void testRemove() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>();
+		for (int i = 0; i < 6; i++) {
+			list.add(i, i);
+		}
+		list.remove(4);
+		list.remove(4);
+		list.remove(4);
 
-  @Test
-  public void testAdd() {
-    MyArrayList<Integer> list = new MyArrayList<Integer>(20);
-    for(int i = 0;i < 30;i++) {
-      list.add(i,i);
-    }
-    assertEquals(list.size(), 30);
-  }
-  @Test
-  public void testRemove() {
-    MyArrayList<Integer> list = new MyArrayList<Integer>();
-    for(int i = 0;i < 6;i++) {
-      list.add(i,i);
-    }
-    list.remove(4);
-    list.remove(4);
-    list.remove(4);
-    
-    assertEquals(list.toString(), "[0, 1, 2]");
-  }
-  @Test
-  public void testGet() {
-    MyArrayList<Integer> list = new MyArrayList<Integer>(-2);
-    for(int i = 0;i < 10;i++) {
-      list.add(i,i);
-    }
-   
-    assertEquals(list.get(2).intValue(), 2);
-    assertEquals(list.get(3).intValue(), 3);
-    assertEquals(list.get(9).intValue(), 9);
-    
-  }
-  
-  @Test
-  public void testSet() {
-    MyArrayList<Integer> list = new MyArrayList<Integer>();
-    list.add(0,-1);
-    list.add(1,-2);
-    
-    list.set(0, 0);
-    list.set(1, 2);
-    
-    assertTrue(list.get(1) == 2);
-  }
-  
+		assertEquals(list.toString(), "[0, 1, 2]");
+	}
+
+	@Test
+	public void testGet() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>(-2);
+		for (int i = 0; i < 10; i++) {
+			list.add(i, i);
+		}
+
+		assertEquals(list.get(2).intValue(), 2);
+		assertEquals(list.get(3).intValue(), 3);
+		assertEquals(list.get(9).intValue(), 9);
+
+	}
+
+	@Test
+	public void testSet() {
+		MyArrayList<Integer> list = new MyArrayList<Integer>();
+		list.add(0, -1);
+		list.add(1, -2);
+
+		list.set(0, 0);
+		list.set(1, 2);
+
+		assertTrue(list.get(1) == 2);
+	}
+
 }
--- src/test/java/com/generalthink/common/datastructure/MyLinkedListTest.java
+++ src/test/java/com/generalthink/common/datastructure/MyLinkedListTest.java
@@ -9,92 +9,93 @@ import org.junit.Test;
 
 public class MyLinkedListTest {
 
-  private MyLinkedList<Integer> list;
-  
-  @Before
-  public void setUp() throws Exception {
-    
-    list = new MyLinkedList<Integer>();
-    for(int i = 0;i < 5;i++) {
-      list.add(i,i);
-    }
-  }
+	private MyLinkedList<Integer> list;
 
-  @Test
-  public void testAdd() {
-    list.add(5, 5);
-    list.add(0, -1);
-    assertTrue(list.size() == 7);
-    assertTrue(list.toString().equals("[-1,0,1,2,3,4,5]"));
-  }
+	@Before
+	public void setUp() throws Exception {
 
-  @Test
-  public void testGet() {
-    assertTrue(list.get(0).equals(0));
-    assertTrue(list.get(1).equals(1));
-    assertTrue(list.get(2).equals(2));
-    assertTrue(list.get(3).equals(3));
-    assertTrue(list.get(4).equals(4));
-    try {
-      list.get(5);
-    } catch (IndexOutOfBoundsException e) {
-      fail("IndexOutOfBounds");
-    }
-    
-  }
-  
-  @Test
-  public void testRemove() {
-    list.remove(2);
-    assertTrue(list.size() == 4);
-    assertTrue(list.get(0) == 0);
-    assertTrue(list.get(1) == 1);
-    assertTrue(list.get(2) == 3);
-    assertTrue(list.get(3) == 4);
-  }
-  
-  @Test
-  public void testSet() {
-    list.set(0, -1);
-    assertTrue(list.size() == 5);
-    assertTrue(list.get(0) == -1);
-    try {
-      list.set(5,5);
-    } catch (Exception e) {
-      fail("IndexOutOfBounds");
-    }
-  }
-  
-  @Test
-  public void testIterator() {
-    Iterator<Integer> it = list.iterator();
-    try {
-      it.next();
-    } catch (Exception e) {
-      fail(e.toString());
-    }
-  }
-  @Test
-  public void testRemoveUseIterator() {
-	Iterator<Integer> it = list.iterator();
-	while(it.hasNext()) {
-		it.next();
-		it.remove();
+		list = new MyLinkedList<Integer>();
+		for (int i = 0; i < 5; i++) {
+			list.add(i, i);
+		}
 	}
-	assertTrue(list.size() == 0);
-  }
-  
-  @Test
-  public void testIteratorModifi() {
-    Iterator<Integer> it = list.iterator();
-    while(it.hasNext()) {
-      System.out.println(it.next());
-      try {
-        list.add(0, 10);
-      } catch (Exception e) {
-        fail("ConcurrentModifiy Exception");
-      }
-    }
-  }
-  
+
+	@Test
+	public void testAdd() {
+		list.add(5, 5);
+		list.add(0, -1);
+		assertTrue(list.size() == 7);
+		assertTrue(list.toString().equals("[-1,0,1,2,3,4,5]"));
+	}
+
+	@Test
+	public void testGet() {
+		assertTrue(list.get(0).equals(0));
+		assertTrue(list.get(1).equals(1));
+		assertTrue(list.get(2).equals(2));
+		assertTrue(list.get(3).equals(3));
+		assertTrue(list.get(4).equals(4));
+		try {
+			list.get(5);
+		} catch (IndexOutOfBoundsException e) {
+			fail("IndexOutOfBounds");
+		}
+
+	}
+
+	@Test
+	public void testRemove() {
+		list.remove(2);
+		assertTrue(list.size() == 4);
+		assertTrue(list.get(0) == 0);
+		assertTrue(list.get(1) == 1);
+		assertTrue(list.get(2) == 3);
+		assertTrue(list.get(3) == 4);
+	}
+
+	@Test
+	public void testSet() {
+		list.set(0, -1);
+		assertTrue(list.size() == 5);
+		assertTrue(list.get(0) == -1);
+		try {
+			list.set(5, 5);
+		} catch (Exception e) {
+			fail("IndexOutOfBounds");
+		}
+	}
+
+	@Test
+	public void testIterator() {
+		Iterator<Integer> it = list.iterator();
+		try {
+			it.next();
+		} catch (Exception e) {
+			fail(e.toString());
+		}
+	}
+
+	@Test
+	public void testRemoveUseIterator() {
+		Iterator<Integer> it = list.iterator();
+		while (it.hasNext()) {
+			it.next();
+			it.remove();
+		}
+		assertTrue(list.size() == 0);
+	}
+
+	@Test
+	public void testIteratorModifi() {
+		Iterator<Integer> it = list.iterator();
+		while (it.hasNext()) {
+			System.out.println(it.next());
+			try {
+				list.add(0, 10);
+			} catch (Exception e) {
+				fail("ConcurrentModifiy Exception");
+			}
+		}
+	}
+
 }
